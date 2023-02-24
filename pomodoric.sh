94b52cd7149c36f9f20db9d55e50a81be5683d4c
Ulisses <iamurise@gmail.com>
2023-02-24 10:39:08 -0300

    add duration parameter

--- pomodoric.sh
+++ pomodoric.sh
@@ -1,4 +1,5 @@
-notify-send "Have a 5 min break!" "#1 pomodoro 15:00" -i /usr/pomodoric/icon.svg -a Pomodoric -u critical; 
+duration=$1
+notify-send "Have a $duration min break!" "#1 pomodoro 15:00" -i /opt/pomodoric/icon.svg -a Pomodoric -u critical; 
 
 # Minimize all windows
 xdotool key super+d;
