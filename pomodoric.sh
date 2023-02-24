c4a3976476f025e3167985795b12f04babf4902c
Ulisses <iamurise@gmail.com>
2023-02-24 11:29:37 -0300

    try using XDG_RUNTIME_DIR to get GUI access

--- pomodoric.sh
+++ pomodoric.sh
@@ -1,5 +1,5 @@
 # turn on graphical interface access
-export $(dbus-launch)
+XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 duration=$1
 notify-send "Have a $duration min break!" "#1 pomodoro 15:00" -i /opt/pomodoric/icon.svg -a Pomodoric -u critical; 
