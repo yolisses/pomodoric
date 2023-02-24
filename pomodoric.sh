ded3722e4be376867340a7cf5a7fdc620601d926
Ulisses <iamurise@gmail.com>
2023-02-24 11:12:51 -0300

    add dbus-launch

--- pomodoric.sh
+++ pomodoric.sh
@@ -1,5 +1,5 @@
 # turn on graphical interface access
-export DISPLAY=:0
+export $(dbus-launch)
 
 duration=$1
 notify-send "Have a $duration min break!" "#1 pomodoro 15:00" -i /opt/pomodoric/icon.svg -a Pomodoric -u critical; 
