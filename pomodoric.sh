2b3244bb933f86eb60fead4e8f691e80181fdba2
Ulisses <iamurise@gmail.com>
2023-02-24 13:15:42 -0300

    use forced timeout

--- pomodoric.sh
+++ pomodoric.sh
@@ -1,9 +1,17 @@
+duration=$1
+back=$2
+
 # turn on graphical interface access
 export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
-duration=$1
-notify-send "Have a $duration min break!" "#1 pomodoro 15:00" -i /opt/pomodoric/icon.svg -a Pomodoric -u critical; 
+./notify-send.sh/notify-send.sh \
+"Have a $duration min break" \
+"See you at $back" \
+-u critical \
+-a Pomodoric \
+-i /opt/pomodoric/icon.svg \
+-t $(($duration * 60 * 1000)) -f \
 
 # Minimize all windows
 xdotool key super+d;
