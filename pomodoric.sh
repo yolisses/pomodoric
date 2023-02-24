9ee06b0f7ab2754270d8532d140fcd0833bfb622
Ulisses <iamurise@gmail.com>
2023-02-24 13:38:10 -0300

    lint

--- pomodoric.sh
+++ pomodoric.sh
@@ -6,12 +6,12 @@ export DISPLAY=:0
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 
 ./notify-send.sh/notify-send.sh \
-"Have a $duration min break" \
-"See you at $back" \
--u critical \
--a Pomodoric \
--i /opt/pomodoric/icon.svg \
--t $(($duration * 60 * 1000)) -f \
+    "Have a $duration min break" \
+    "See you at $back" \
+    -u critical \
+    -a Pomodoric \
+    -i /opt/pomodoric/icon.svg \
+    -t $(($duration * 60 * 1000)) -f
 
 # Minimize all windows
-xdotool key super+d;
+xdotool key super+d
