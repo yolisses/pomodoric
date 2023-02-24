154944762dd56114b64f159d314d503a5c4d98bf
Ulisses <iamurise@gmail.com>
2023-02-24 10:57:28 -0300

    add export display

--- pomodoric.sh
+++ pomodoric.sh
@@ -1,3 +1,6 @@
+# turn on graphical interface access
+export DISPLAY=:0
+
 duration=$1
 notify-send "Have a $duration min break!" "#1 pomodoro 15:00" -i /opt/pomodoric/icon.svg -a Pomodoric -u critical; 
 
